757eb70269bcfe241923c0e5df0b07b2b1400489
leozdgao <gzdleo0816@gmail.com>
2015-01-19 12:04:54 +0800

    add db connection handle and add static serve

--- app.js
+++ app.js
@@ -7,21 +7,40 @@ var Promise = require("bluebird");
 var mongoose = require("mongoose");
 Promise.promisifyAll(mongoose);
 
+// load config
+var config = require('./config.json');
+
 app.use(require('morgan')('dev'));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/user', require('./routes/user'));
 app.use('/rest', require('./routes/rest'));
+// serve static files
+var serveStatic = require('serve-static');
+var contentDesposition = require('content-disposition');
+app.use('/download', serveStatic(config.staticFolder, {
+    setHeaders: function(res, path) {
+
+        res.setHeader('Content-Disposition', contentDesposition(path));
+    }
+}));
+
+app.use(function(req, res, next) {
+
+    var err = new Error("Not found.");
+    err.status = 404;
+    next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
 
     console.log(err);
-    res.status(500).json(err);
+    res.status(err.status || 500).json(err);
 });
 
-var config = require('./config.json');
+
 var port = process.env.PORT || config.port || 4000;
 
 // set db connectiion config, timeout 5s
@@ -31,20 +50,34 @@ var dbConfig = {
     }
 };
 
-mongoose.connectAsync(config.dbConnection, dbConfig)
-    .then(function() {
+mongoose.connect(config.dbConnection, dbConfig);
 
-        app.listen(port, function() {
+mongoose.connection.on("connected", function() {
 
-            console.log('Express server listening on port ' + port);
-        });
-    })
-    .catch(function(err) {
-        
-        console.log(err.err || err.message || "Connect failed.");
-    });
-    
+    console.log("Connected to DB...");
+    connected = true;
+});
+
+mongoose.connection.on("disconnected", function() {
+
+    // after a successful connecting, mongoose will reconnect automatically if connection disconnected.
+    if(!connected) {
+
+        console.log("DBConnection closed. Try to reconnect.");
+
+        setTimeout(function() {
+
+            mongoose.connection.open(config.dbConnection, dbConfig);
+        }, 5000);   
+    }
+});
+
 mongoose.connection.on("error", function(err) {
 
-    console.log(err.err || err.message || "Error occurred on connection.");
+    console.log("Error occurred: " + err.message);
+});
+
+app.listen(port, function() {
+
+    console.log("Server listening on port " + port);
 });
